dc5396ae6b26625efdb49fd35dfff78f81026976
Abdul <am726016@gmail.com>
2025-03-06 09:57:17 +0000

    Improve parsing, fix bug with speakers with spaces, improve choice menu, comment recognition and more.

--- markdown-to-renpy.py
+++ markdown-to-renpy.py
@@ -33,18 +33,21 @@ def text_script(line:str) -> str:
             text =  "\"" + text + "\"" + "\n"
     return text
 
-def defined_character_script(line:str, character:str, character_tag:str) -> str:
+def defined_character_script(line:str, character_tag:str, character_name:str) -> str:
     """
     Returns a string in the format
     `character "text"`
     """
-    split_line = line.split()
+    split_line = line.lstrip(character_name)
+    split_line = split_line.lstrip(" :").split()
     # Process speaker.
-    split_line.pop(0)
     speaker = character_tag
 
     # Process text.
-    text = text_script(split_line)
+    joint_line = ""
+    for word in split_line:
+        joint_line = joint_line + " " + word
+    text = text_script(joint_line.strip())
 
     # Concatenate and format the speaker and the text.
     line = speaker + " " + text
@@ -134,6 +137,7 @@ except IndexError:
     input("Press Enter to close the program.")
     sys.exit(0)
 
+
 output_filename = file_path.split("/")[-1]
 output_filename = output_filename.replace(".txt", ".rpy")
 with open(file_path,'r', encoding='utf-8-sig') as file:
@@ -154,97 +158,113 @@ except:
     except:
         print("No characters.rpy file found. No problem, we'll just put every speaker as a string.")
         characters_file = ""
-
-characters_file = characters_file.split("\n")
-
+    characters_file = characters_file.split("\n")
 
 
 # Parse the characters file
 all_characters = {}
 for line in characters_file:
-    line = line.replace(" ", "")
     if line.strip().startswith('define'):
         if 'Character("' in line:
             # Grab the variable name of the character
             line = line.replace("define", "")
-            index = line.find('=')
-            character_variable = line[:index]
 
-            # Grab the full name of the character
-            character_full_name = line[index + 12:]
-            index = character_full_name.find('"')
-            character_full_name = character_full_name[:index]
-
+            character_variable = line.split('=')[0].strip()
+            # grab the full name
+            character_full_name = line.split('"')[1].strip()
+        
+            # Store them in a dict.
             all_characters[character_variable] = character_full_name
 
 
 
+# Custom configuration will parse the text and make edits before anything else.
+allow_custom_edits = True
+if allow_custom_edits:
+    print("Custom edits applied.")
+    # replace_strings = {"***": "\n",}
+    # comment_symbol = "*" # Anything wrapped in this symbol will be treated as a comment.
 
 
 
 
 # Process the input file and write to the output text file.
 with open(output_filename, "w", encoding = "utf-8-sig") as output_file:
+
+
     for line in file:
         line_processed = False
 
-        # If the line is blank, just insert a blank line to maintain the paragraph spacing.
-        if line == '':
-            output_file.write('\n')
-
-        # Comments are marked with %%, so convert them into Python comments.
-        elif line.strip().startswith('%%'):
-            line = '# ' + line + '\n'
-            output_file.write(line)
-
-        # Lines beginning with a '#' are labels
-        elif line[0] == "#":
-            line = line.removeprefix("#")
-            line = line.removeprefix("#")
-            line = line.removeprefix(" ")
-            line = line.removesuffix("\n")
-            line = line.replace(" ", "_")
-            line = line.replace(".", "")
-            line = " " + line
-            line = line.lower()
-            line = "label" + line.removeprefix("#").removesuffix("\n") + ":"
-        
-        # This keyword is used to create a choice menu
-        elif '**(Choice)**' in line:
-            line = "    menu:\n"
-        
-        # For choices, anything matching the pattern `**number` is a choice 
-        elif bool(re.match(r'^\*\*\d+', line)): # I'm not even going to begin to pretend to know what black magic this is. 
-            line = '        "' + line + '":\n'
-
-        elif '---' in line:
-            line = "    scene clear\n"
+        # Parse the text and make custom edits.
+        if allow_custom_edits:
+            if line == "***":
+                line_processed = True
+            line = line.replace("***", "\nscene [insert_scene]\n")
+            if re.fullmatch(r"\*[^*]+\*+", line):
+                line = f"# {line.strip('*')}"
+                line_processed = True
+            
+        if line_processed:
+            pass
 
         else:
-            # Convert all markdown syntax into RenPy
-            line = markdown_to_renpy(line)
 
-            # Check if the speaker is a defined character.
-            for character in all_characters:
-                if character in line:
-                    line = defined_character_script(line, character, all_characters[character])
-                    line_processed = True
+            # If the line is blank, just insert a blank line to maintain the paragraph spacing.
+            if line == '':
+                output_file.write('\n')
 
-            if line_processed == False:
-                # Check if the speaker is an undefined character
-                if ":" in line:
-                    line = undefined_character_script(line)
-                # Check if the line is an empty line
-                elif line == "":
-                    pass
-                # The speaker is the narrator.
-                else:
-                    line = line.strip(" ")
-                    line = speech_marks_handling(line)
-                    line = "\"" + line + "\"" + "\n"
+            # Comments are marked with %%, so convert them into Python comments.
+            elif line.strip().startswith('%%'):
+                line = '    # ' + line + '\n'
+
+            # Lines beginning with a '#' are labels
+            elif line[0] == "#":
+                line = line.removeprefix("#")
+                line = line.removeprefix("#")
+                line = line.removeprefix(" ")
+                line = line.removesuffix("\n")
+                line = line.replace(" ", "_")
+                line = line.replace(".", "")
+                line = " " + line
+                line = line.lower()
+                line = "label" + line.removeprefix("#").removesuffix("\n") + ":"
             
-            # Indent lines to 4 spaces
-            line = "    " + line
+            # This keyword is used to create a choice menu
+            elif '**(Choice)**' in line:
+                line = "    menu:\n"
+            
+            # For choices, anything matching the pattern `**number` is a choice 
+            elif bool(re.match(r'^\*\*\d+', line)): # I'm not even going to begin to pretend to know what black magic this is. 
+                line = '        "' + line + '":\n'
+
+            elif '---' in line:
+                line = "    scene clear\n"
+
+            else:
+                # Convert all markdown syntax into RenPy
+                line = markdown_to_renpy(line)
+
+                # Check if the speaker is a defined character.
+                for tag, character in all_characters.items():
+                    if line.startswith(character):
+                        line = defined_character_script(line, tag, character)
+                        line_processed = True
+
+                if line_processed == False:
+                    # Check if the speaker is an undefined character
+                    if ":" in line:
+                        line = undefined_character_script(line)
+                    # Check if the line is an empty line
+                    elif line == "":
+                        pass
+                    # The speaker is the narrator.
+                    else:
+                        line = line.strip(" ")
+                        line = speech_marks_handling(line)
+                        line = "\"" + line + "\"" + "\n"
+                
+                # Indent lines to 4 spaces
+                line = "    " + line
         
         # Write line to file.
         output_file.write(line)
